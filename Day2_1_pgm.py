bf9ad7f13e85cb6857e4082f823717df40241382
Aishwarya M N <aishwaryamn168@gmail.com>
2026-09-22 22:45:16 +0530

    Add initial Python program with personal details

---
+++ Day2_1_pgm.py
@@ -0,0 +1,13 @@
+# Write your Python code here
+name="aishu"
+age=20
+city="bangalore"
+course="python"
+marks=89
+print("name is:" ,name)
+print("age is:",age)
+print("city is:",city)
+print("course is:",course)
+print("marks is:",marks)
+
+
