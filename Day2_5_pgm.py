8c8452b7e4970dbb2bfb896d468848c650e6271a
Aishwarya M N <aishwaryamn168@gmail.com>
2026-09-22 23:02:10 +0530

    Add eligibility check based on marks and attendance

---
+++ Day2_5_pgm.py
@@ -0,0 +1,7 @@
+# Write your Python code here
+marks=int(input("enter marks:"))
+attendence=int(input("enter attendance:"))
+if marks>=80 and attendence>=75:
+    print("eligible")
+else:
+    print("not eligible")
