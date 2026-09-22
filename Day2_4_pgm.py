654322088e24bd3cc9b0cb3326056b1266dc65bb
Aishwarya M N <aishwaryamn168@gmail.com>
2026-09-22 22:55:19 +0530

    Add string manipulation examples in Day2_4_pgm.py

---
+++ Day2_4_pgm.py
@@ -0,0 +1,6 @@
+s = "Python Programming"
+print("First character:", s[0])
+print("Last character:", s[-1])
+print("First five characters:", s[:5])
+print("Reversed string:", s[::-1])
+print("Uppercase string:", s.upper())
